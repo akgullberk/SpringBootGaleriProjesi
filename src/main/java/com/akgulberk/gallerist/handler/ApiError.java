2ffd3955ce23befbebdc1b6db7bab3f9da43a7aa
Berk Akgül <berkak_gul51@hotmail.com>
2025-12-28 19:03:27 +0300

    Fırlatılan Exceptionların Yakalanması

---
+++ src/main/java/com/akgulberk/gallerist/handler/ApiError.java
@@ -0,0 +1,15 @@
+package com.akgulberk.gallerist.handler;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiError<E> {
+
+    private Integer status;
+
+    private Exception<E> exception;
+}
